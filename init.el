704dc7786bb26fd92b5ede74fd3b704065f9918e
stevechan <chanshunli@gmail.com>
2022-10-08 15:22:09 +0800

    不加载eaf

--- init.el
+++ init.el
@@ -433,26 +433,3 @@
 ;;(setq mac-option-modifier 'none)
 ;;(setq mac-command-modifier 'meta)
 
-;; https://github.com/emacs-eaf/emacs-application-framework/wiki/macOS
-(setq eaf--mac-enable-rosetta t)
-(add-to-list 'load-path "~/OtherPro/emacs-application-framework")
-(require 'eaf)
-(use-package eaf
-  :load-path "~/OtherPro/emacs-application-framework"
-  :custom
-  ; See https://github.com/emacs-eaf/emacs-application-framework/wiki/Customization
-  (eaf-browser-continue-where-left-off t)
-  (eaf-browser-enable-adblocker t)
-  (browse-url-browser-function 'eaf-open-browser)
-  :config
-  (defalias 'browse-web #'eaf-open-browser)
-  ;;(eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
-  ;;(eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
-  ;;(eaf-bind-key take_photo "p" eaf-camera-keybinding)
-  ;; (eaf-bind-key nil "M-q" eaf-browser-keybinding)
-) ;; unbind, see more in the Wiki
-
-(require 'eaf-browser)
-(require 'eaf-pdf-viewer)
-;;;;;;;;
-
